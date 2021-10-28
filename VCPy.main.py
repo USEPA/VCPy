2e4dca18424a5d8eb8f26958645988d99013eb97
Karl Seltzer <seltzer.karl@epa.gov>
2021-10-28 14:52:41 -0400

    updated 2009/2010

--- VCPy.main.py
+++ VCPy.main.py
@@ -13,8 +13,8 @@ startTime = datetime.now()
 ####################################################################################################
 ### User Input
 ### Start and end year for execution:
-STARTYEAR = '2018'
-ENDYEAR   = '2019'
+STARTYEAR = '2009'
+ENDYEAR   = '2010'
 ### Generate summary figures (TRUE or FALSE)?
 GEN_FIGS  = 'FALSE'
 ### Generate SMOKE flat files (TRUE or FALSE)? Note: significantly increases execution time.
