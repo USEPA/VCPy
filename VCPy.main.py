31c24e0ae4fb6139e0e026b15414084aeccb3fe1
Karl Seltzer <seltzer.karl@epa.gov>
2022-04-28 20:28:41 -0400

    Updated with 2020 data

--- VCPy.main.py
+++ VCPy.main.py
@@ -13,12 +13,12 @@ startTime = datetime.now()
 ####################################################################################################
 ### User Input
 ### Start and end year for execution:
-STARTYEAR = '2016'
-ENDYEAR   = '2019'
+STARTYEAR = '2019'
+ENDYEAR   = '2020'
 ### Generate summary figures (TRUE or FALSE)?
 GEN_FIGS  = 'FALSE'
 ### Generate SMOKE flat files (TRUE or FALSE)? Note: substantially increases execution time.
-GEN_FF10  = 'FALSE'
+GEN_FF10  = 'TRUE'
 ### Location of modules:
 sys.path.append('./modules/')
 ### Outdoor evaporation timescale parameters. See Section 2.1.5 of Seltzer et al. 2021 Atmos Chem Phys for more details.
